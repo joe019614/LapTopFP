457642effa2f4b18c5cf14159548f7a81e988c1a
joe019614 <choichilam28@gmail.com>
2023-10-03 11:49:13 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+/* import {Route,Switch} from "react-router-dom"; */
 import LandingPage from "./LandingPage/LandingPage";
+import Search from "./Search/Search";
+import {Routes,Route} from "react-router-dom";
 
 
 function App() {
   return (
-  <LandingPage/>
+
+    <switch>
+        <Routes>
+          <Route path="/search" Component={Search}/>
+          <Route path="/"       Component={LandingPage}/>
+        </Routes>
+    </switch>
+
+
   );
 }
 
---
+++ src/Search/Search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div>
+      Search12312313211321
+    </div>
+  )
+}
