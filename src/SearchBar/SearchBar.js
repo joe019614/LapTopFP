c8a1b824e3ebe6385f98f92fe5778f068ccc2db5
joe019614 <choichilam28@gmail.com>
2023-10-03 10:42:21 +0800

    add search suggestions

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -11,12 +11,18 @@ export default function SearchBar() {
             <p className="control">
                 <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Chinese,Japanese,Burger,Pizza"/>
             </p>
-             <p className="control">
+            <p className="control">
                 <button className="button is-static is-medium">Near</button>   
             </p>
-             <p className="control">
+            <p className="control">
                 <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="where"/>
             </p>
+            <button className={`button is-medium ${styles['search-button']}`}>
+                <span className={`icon is-small ${styles['search-icon']}`}>
+                <i className="fas fa-search" /* style={{color: "#ffffff",}} */ ></i>
+                </span>
+            </button>
+            
         </div>
     </div>
   );
