abdae5f4b14e8e71e54ac4a128734a4eba72e445
joe019614 <choichilam28@gmail.com>
2023-10-03 12:19:57 +0800

    add NavBar

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from '../NavBar/NavBar'
 
 export default function Search() {
   return (
     <div>
-      Search12312313211321
+        <NavBar/>
     </div>
   )
 }
